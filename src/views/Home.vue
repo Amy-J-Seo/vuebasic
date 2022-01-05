277746c63422c1a9d40bb1afa34a9f89cdb9e530
Cbear1234 <amywonderland77@gmail.com>
2022-01-05 22:20:29 +0900

    select box value binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,69 @@
 <template>
     <div>
-        <h1>Welcom to home</h1>
+        <h1>Welcom to {{title2}}</h1>
+        <input type="text" v-model="input1">
+        <button @click="getData">Click me</button>
+         <button @click="setData">set me</button>
+
+         <select class="form-control" v-model="region">
+             <option v-for="(d, i) in options" :key="i" :value="d.v">{{d.t}}</option>
+         </select>
     </div>
 </template>
 
 <script>
 export default {
-    name:'Home'
+    name:'Home',
+    data(){
+        return{
+            title:"Hello Hambo",
+            title2:"Seoul",
+            input1:"abcd",
+            options:[
+                {v:"s", t:"seoul"},
+                {v:"d", t:"busan"},
+                {v:"f", t:"ded"}
+            ],
+            region:"d"
+        }
+    },
+    watch:{
+        input1(){
+            console.log(this.input1);
+        }
+    },
+    methods:{
+        getData(){
+            alert(this.input1)
+        },
+        setData(){
+            this.input1 = "1234";
+        },
+        
+    },
+    beforeCreate(){
+            console.log('beforeCreate');
+        },
+        created(){
+            console.log('created');
+        },
+        beforeMount(){
+            console.log('beforeMount');
+        },
+        mounted(){
+            console.log('mounted');
+        },
+        beforeUpdate(){
+            console.log('beforeUpdate');
+        },
+        updated(){
+            console.log('updated');
+        },
+        beforeDestroy(){
+            console.log('beforeDestroy');
+        },
+        destroyed(){
+            console.log('destroyed');
+        }
 }
 </script>
